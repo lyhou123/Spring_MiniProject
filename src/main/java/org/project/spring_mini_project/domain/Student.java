70a7bef21301ef2c1e45f69231a25b0a092ec61c
lyhou123 <lyhou282@gmail.com>
2024-04-12 15:28:19 +0700

    clear domain

--- src/main/java/org/project/spring_mini_project/domain/Student.java
+++
@@ -1,9 +0,0 @@
-package org.project.spring_mini_project.domain;
-
-public class Student {
-    private Integer id;
-    private String high_school;
-    private Boolean is_blocked;
-    private String university;
-    private Long user_id;
-}
