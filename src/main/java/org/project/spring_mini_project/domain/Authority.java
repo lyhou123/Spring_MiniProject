12c8656773d29a7964d350ed843ff02646107715
lyhou123 <lyhou282@gmail.com>
2024-04-17 16:08:15 +0700

    clear file before conflict

--- src/main/java/org/project/spring_mini_project/domain/Authority.java
+++
@@ -1,6 +0,0 @@
-package org.project.spring_mini_project.domain;
-
-public class Authority {
-    private Integer id;
-    private String name;
-}
--- src/main/java/org/project/spring_mini_project/domain/Role.java
+++
@@ -1,6 +0,0 @@
-package org.project.spring_mini_project.domain;
-
-public class Role {
-    private long id;
-    private String name ;
-}
